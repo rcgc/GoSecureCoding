aedfdea61a98668bf7e5dd2dbd1a39d3631323a8
rcgc <roberto140298@gmail.com>
2023-01-14 15:12:19 -0600

    size limiting finished

---
+++ size_limiting/httpd.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func logHandler(w http.ResponseWriter, r *http.Request){
+	defer r.Body.Close()
+
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "can't read", http.StatusBadRequest)
+		return
+	}
+
+	// TODO: Save in database
+
+	fmt.Fprintf(w, "%d bytes stored\n", len(data))
+}
+
+func main() {
+	http.HandleFunc("/log", logHandler)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
